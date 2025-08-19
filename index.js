1e86af00a5c60f7ac07d9b03cb64bcf2b1c8400e
sayo <sayoejiba@gmail.com>
2025-08-19 10:18:48 +0100

    counter section modify

--- index.js
+++ index.js
@@ -1,60 +1,52 @@
-let count=0;
+let count = 0;
 let interval;
 
-const countlabel=document.getElementById("countlabel");
-const increasebtn=document.getElementById('increasebtn');
-const decreasebtn=document.getElementById("decreasebtn");
-const reset=document.getElementById("reset");
+const countlabel = document.getElementById("countlabel");
+const increaseBtn = document.getElementById("increaseBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const reset = document.getElementById("reset");
 
-function updatedisplay(){
-    countlabel.innerHTML= count;
+function updatedisplay() {
+  countlabel.innerHTML = count;
 }
 updatedisplay();
 
 function startcounting(changefn){
-    changefn()
-    interval=setInterval(changefn, 100);
-
+  changefn();
+  interval = setInterval(changefn, 100);
 }
 
 function stopcounting(){
-    clearInterval(interval);
+  clearInterval(interval);
 }
 
 function buttonHoldUpdate(button, changefn){
-    button.addEventListener ('mousedown', () => startcounting(changefn));
-    button.addEventlistner ('mouseup', stopcounting);
-    button.addEventlistner ("mouseleave", stopcounting);
-    
+  button.addEventListener
+  button.addEventListener("mousedown", () => startcounting(changefn));
+  button.addEventListener("mouseup", stopcounting);
+  button.addEventListener("mouseleave", stopcounting);
 
-    button.addEventlistner ('touchstart',(e) =>{
-        e.prevetdefault();
-        startcounting(changefn);
-    })
+  button.addEventListener("touchstart", (e) =>{
+    e.preventDefault();
+    startcounting(changefn);
+  });
+  
+  button.addEventListener("touchend", stopcounting);
+}
 
-    button.addEventlistner("touchend" ,stopcounting);
-    }
-    
+buttonHoldUpdate(increaseBtn, () => {
+  count++;
+  updatedisplay();
+});
 
-    buttonHoldUpdate(increasebtn, () => {
-    count++;
-    updatedisplay();
-    })
+buttonHoldUpdate(decreaseBtn, () => {
+  if (count > 0) {
+    count--;
+  }
+  updatedisplay();
+});
 
-    buttonHoldUpdate(decreasebtn,() =>{
-        if(count > 0){
-            count--;
-        }
-        updatedisplay();
-    })
-
-    reset.addEventListener('click', () =>{
-        count=0;
-        updatedisplay();
-    })
-
-
-    
-
-    
-
+reset.addEventListener("click", () => {
+  count = 0;
+  updatedisplay();
+})
